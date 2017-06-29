7f00263f65463a3ce9d30edda533a3ea655ffe72
Lauana <Lauana>
2017-06-29 19:10:15 -0300

    Formulario de contato criado e rodando

--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -3,16 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ContatoComponent } from './contato/contato.component';
-import{ ReactiveFormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpModule} from '@angular/http';
+//import { HomeComponent } from './home/home.component';
+//import { RouterModule } from '@angular/router';
+//import { ArtigoComponent } from './artigo/artigo.component';
+////import { routes } from './router';
+//import { ChatComponent } from './chat/chat.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
-    ContatoComponent
+    ContatoComponent,
+    //HomeComponent,
+    //ArtigoComponent,
+    //ChatComponent
   ],
   imports: [
-    BrowserModule,
-    ReactiveFormsModule,HttpModule
+    BrowserModule, ReactiveFormsModule, HttpModule, //RouterModule.forRoot(routes), FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
